1ab8e7aff235dce1ee6264fbcd194dbda511f104
Matias Saguir <mativs@gmail.com>
2013-02-21 19:09:02 -0300

    fix some groups fixes

--- public/js/header.js
+++ public/js/header.js
@@ -7,5 +7,9 @@ $( document ).ready( function() {
 	var hideHeader = function(eventObject) {
 		$(this).animate({"height":95},1000, 'easeInOutQuart');
 	}
-	$('.branding-data').hoverIntent(showHeader, hideHeader)
+	$('.branding-data').hoverIntent({    
+		     over: showHeader, // function = onMouseOver callback (REQUIRED)    
+		     timeout: 1000, // number = milliseconds delay before onMouseOut    
+		     out: hideHeader // function = onMouseOut callback (REQUIRED)    
+		});
 });
