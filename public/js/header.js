6af2a8f9b3a8ed3c1eb44161ab09de56234e8dd0
Matias Saguir <mativs@nowherefarm.com>
2013-02-22 18:27:15 -0300

    fix hover on header to title

--- public/js/header.js
+++ public/js/header.js
@@ -1,13 +1,13 @@
 $( document ).ready( function() {
 	var showHeader = function (eventObject) {
-		$(this).animate({"height":600},1000, 'easeInOutQuart');
+		$(eventObject.target.parentNode.parentNode.parentNode.parentNode).animate({"height":600},1000, 'easeInOutQuart');
 		
 	};
 
 	var hideHeader = function(eventObject) {
-		$(this).animate({"height":95},1000, 'easeInOutQuart');
+		$(eventObject.target.parentNode.parentNode.parentNode.parentNode).animate({"height":95},1000, 'easeInOutQuart');
 	}
-	$('.branding-data').hoverIntent({    
+	$('.branding-data .title').hoverIntent({    
 		     over: showHeader, // function = onMouseOver callback (REQUIRED)    
 		     timeout: 1000, // number = milliseconds delay before onMouseOut    
 		     out: hideHeader // function = onMouseOut callback (REQUIRED)    
