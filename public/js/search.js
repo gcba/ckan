db94b9a498282a20e92f119e5b3066918765fa6e
Matias Saguir <mativs@gmail.com>
2013-03-01 15:26:41 -0300

    fix dataset search

--- public/js/search.js
+++ public/js/search.js
@@ -28,200 +28,292 @@ function initSearch() {
 	};
 }
 
-function pushUrl(facets, query_str, sort_str) {
-	$.bbq.pushState( $.param( { 
-		tags: facets['tags'].length > 0 ? "." + facets['tags'].join('.'): "",
-		groups: facets['groups'].length > 0 ? "." + facets['groups'].join('.'): "",
-		res_format: facets['res_format'].length > 0 ? "." + facets['res_format'].join('.'): "",
-		query: query_str,
-		sort: sort_str
-	} ));
+function updateMessage() {
+	var hashOptions = $.deparam.fragment();
+	var tag_count = 0;
+	var format_count = 0;
+	var group_count = 0;
+	
+	if (hashOptions.tags) {
+		var filters = hashOptions.tags.split('.')
+		tag_count = filters.length - 1;
+	}		
+	
+	if (hashOptions.res_format) {
+		var filters = hashOptions.res_format.split('.')
+		format_count = filters.length - 1;
+	}
+	
+	if (hashOptions.groups) {
+		var filters = hashOptions.groups.split('.')
+		group_count = filters.length - 1;
+	}
+
+	final_str = "";
+	if (tag_count > 0 ) {
+		final_str = tag_count + (tag_count > 1 ? " tags" : " tag");
+	}
+	if (format_count > 0 ) {
+		var format_str = format_count + (format_count > 1 ? " formatos" : " formato");
+		final_str += final_str.length > 0 ? ", " + format_str : format_str
+	}
+	if (group_count > 0 ) {
+		var group_str = group_count + (group_count > 1 ? " grupos" : " grupo");
+		final_str += final_str.length > 0 ? ", " + group_str : group_str;
+	}
+	var total = $('.dataset').length;
+	var cantidad = total - $('.dataset.isotope-hidden').length;
+	if (final_str.length > 0 ){
+		final_str = "Seleccionaste <strong>" + final_str +"</strong>. Viendo <strong>" + cantidad + "</strong> datasets de un total de <strong>" + total +"</strong>."
+	} else {
+		final_str += "Estás viendo todos los datasets. "
+	}
+	$('.tags-status p').html(final_str);
+}
+
+function filterDatasets() {
+	var hashOptions = $.deparam.fragment();
+	filter=""
+	if (hashOptions.tags) {
+		filter += hashOptions.tags;
+	}		
+	if (hashOptions.res_format) {
+		filter += hashOptions.res_format;
+	}
+	if (hashOptions.groups) {
+		filter += hashOptions.groups;
+	}
+	if (hashOptions.query) {
+		if (!hashOptions.filter) {
+			hashOptions.filter = "";
+		}
+		filter += ":contains('" + hashOptions.query + "')"
+	}
+	$('.datasets').isotope( {
+		filter: filter,
+		sortBy : hashOptions.sort 
+	});
 }
 
 function toggleTagBox(ele){
-	tagObj = $('.tagbox [ckan-facet="'+ele+'"]');
-	tagObj.toggle();
+	$('.tagbox [ckan-facet="'+ele+'"]').toggle();
 }
 
 function toggleDropdown(ele) {
-	dropObj = $($('.btn-group [ckan-facet="' + ele + '"]').parent());
-	dropObj.toggle();
+	$($('.btn-group [ckan-facet="' + ele + '"]').parent()).toggle();
+}
+
+function toggleGroupBar(ele) {
+	$('.groups .' + ele + ' a').toggleClass('active');
 }
 
 function toggleGroup(ele) {
-	groupObj = $('.groups .' + group + ' a');
-	groupObj.toggleClass('active');
+	toggleTagBox(ele);
+	toggleGroupBar(ele);
+}
 
+function toggleTag(ele){
+	toggleTagBox(ele);
+	toggleDropdown(ele);
+}
+
+function toggleFormat(ele){
+	toggleTagBox(ele);
+	toggleDropdown(ele);
+}
+
+function toggleQueryText(ele){
+	$('#search').val(ele);
+}
+
+function toggleFacets() {
+	var hashOptions = $.deparam.fragment();
+	
+	if (hashOptions.tags) {
+		var filters = hashOptions.tags.split('.')
+		for ( var i=1; i<filters.length; i++ ) {
+			toggleTag(filters[i]);
+		}
+	}		
+
+	if (hashOptions.res_format) {
+		var filters = hashOptions.res_format.split('.')
+		for ( var i=1; i<filters.length; i++ ) {
+			toggleFormat(filters[i])
+		}
+	}
+
+	if (hashOptions.groups) {
+		var filters = hashOptions.groups.split('.')
+		for ( var i=1; i<filters.length; i++ ) {
+			toggleGroup(filters[i])
+		}
+	}
+
+	if (hashOptions.query) {
+		toggleQueryText(hashOptions.query);
+	}
+}
+
+function clearTagBox(filter){
+	$('.tagbox [ckan-filter="'+filter+'"]').hide();
+}
+
+function clearDropdown(filter) {
+	$($('.btn-group [ckan-filter="' + filter + '"]').parent()).show();
+}
+
+function clearFilter(eventObject) {
+	var filter = $(eventObject.currentTarget).attr('ckan-filter');
+	clearTagBox(filter);
+	clearDropdown(filter);
+	switch(filter){
+		case 'groups':
+			$.bbq.pushState( $.param( {groups: ""} ));
+			break;
+		case 'tags':
+			$.bbq.pushState( $.param( {tags: ""} ));
+			break;
+		case 'res_format':
+			$.bbq.pushState( $.param( {res_format: ""} ));
+			break;
+	}
+	updateMessage();
+	closeDropdowns();
+	filterDatasets();
+	return false;
+}
+
+function toggleUrlGroup(facet) {
+	var hashOptions = $.deparam.fragment();
+	if (hashOptions.groups) {
+		index = hashOptions.groups.indexOf(facet);
+		length = facet.length;
+		if ( index < 0 ) {
+			hashOptions.groups = hashOptions.groups + "." + facet;
+		} else {
+			hashOptions.groups = hashOptions.groups.replace("." + facet, "");
+		}
+	}
+	else {
+		hashOptions.groups = "." + facet;
+	}
+	$.bbq.pushState( $.param( { 
+		groups: hashOptions.groups
+	} ));
+}
+
+function toggleUrlTag(facet) {
+	var hashOptions = $.deparam.fragment();
+	if (hashOptions.tags) {
+		index = hashOptions.tags.indexOf(facet);
+		if ( index < 0 ) {
+			hashOptions.tags = hashOptions.tags + "." + facet;
+		} else {
+			hashOptions.tags = hashOptions.tags.replace("." + facet, "");
+		}
+	}
+	else {
+		hashOptions.tags = "." + facet;
+	}
+	$.bbq.pushState( $.param( { 
+		tags: hashOptions.tags
+	} ));
+}
+
+function toggleUrlFormat(facet) {
+	var hashOptions = $.deparam.fragment();
+	if (hashOptions.res_format) {
+		index = hashOptions.res_format.indexOf(facet);
+		length = facet.length;
+		if ( index < 0 ) {
+			hashOptions.res_format = hashOptions.res_format + "." + facet;
+		} else {
+			hashOptions.res_format = hashOptions.res_format.replace("." + facet, "");
+		}
+	}
+	else {
+		hashOptions.res_format = "." + facet;
+	}
+	$.bbq.pushState( $.param( { 
+		res_format: hashOptions.res_format
+	} ));
+}
+
+function toggleFacetToFilter(eventObject) {
+	var filter = $(eventObject.currentTarget).attr('ckan-filter');
+	var facet = $(eventObject.currentTarget).attr('ckan-facet');
+
+	switch(filter){
+		case 'groups':
+			toggleGroup(facet)
+			toggleUrlGroup(facet)
+			break;
+		case 'tags':
+			toggleTag(facet)
+			toggleUrlTag(facet)
+			break;
+		case 'res_format':
+			toggleFormat(facet)
+			toggleUrlFormat(facet)
+			break;
+	}
+
+	closeDropdowns();
+	filterDatasets();
+	updateMessage();
+	return false;
+}
+
+function closeDropdowns() {
+	$('[data-toggle="dropdown"]').parent().removeClass('open');
+}
+
+function toggleUrlQuery(query) {
+	$.bbq.pushState( $.param( { 
+		query: query
+	} ));
+}
+
+function toggleQuery(eventObject) {
+	kwd = $(this).val();
+	toggleQueryText(kwd);
+	toggleUrlQuery(kwd);
+	updateMessage();
+	filterDatasets();
+	return true;
+}
+
+function toggleUrlSort(sort) {
+	$.bbq.pushState( $.param( { 
+		sort: sort
+	} ));
+}
+
+function toggleSort(eventObject) {
+	sort_str = $(eventObject.currentTarget).attr('ckan-sort');
+	toggleUrlSort(sort_str);
+	filterDatasets();
+	closeDropdowns();
+	updateMessage();
+	return false;
 }
 
 $( document ).ready( function() {
-
-
 	$('.total-view-count, .last-modified').tooltip();
 
 	initSearch();
   	initIsotope();
+  	toggleFacets();
+  	filterDatasets();
+  	updateMessage();
 
-	var facets = {
-		'tags': [],
-		'res_format': [],
-		'groups': []
-	}
-	var query_str = "";
-	var sort_str="original-order";
+  	
 
-	
-
-
-	
-
-
-
-
-	pushTagFormat = function(tag, type){
-		$('.nav-tags-formats .inner').append('<div ckan-filter="'+ type + '" ckan-facet="' + tag + '" class="tags tags-block"><button type="button" class="close" data-dismiss="alert">×</button><p>'+tag+'</p></div> ');
-		$('.nav-tags-formats .inner [ckan-facet="' + tag + '"]').click(toggleFacetToFilter);
-		$($('.btn-group [ckan-facet="' + tag + '"]').parent()).hide();
-	}
-
-	cleanTagFormat = function() {
-		$('.nav-tags-formats .inner').empty();
-		$($('.btn-group [ckan-facet]').parent()).show();
-	}
-
-	cleanGroups = function() {
-		$('.groups li a').removeClass("active");
-	}
-
-	activeGroup = function(group) {
-		$('.groups .' + group + ' a').addClass('active')
-	}
-
-	updateFilterMessage = function (tag_count, format_count, group_count) {
-		final_str = "";
-		if (tag_count > 0 ) {
-			final_str = tag_count + (tag_count > 1 ? " tags" : " tag");
-		}
-		if (format_count > 0 ) {
-			var format_str = format_count + (format_count > 1 ? " formatos" : " formato");
-			final_str += final_str.length > 0 ? ", " + format_str : format_str
-		}
-		if (group_count > 0 ) {
-			var group_str = group_count + (group_count > 1 ? " grupos" : " grupo");
-			final_str += final_str.length > 0 ? ", " + group_str : group_str;
-		}
-		if (final_str.length > 0 ){
-			final_str = "Seleccionaste <strong>" + final_str +"</strong>."
-		} else {
-			final_str += "Estás viendo todos los datasets."
-		}
-		$('.nav-tags-formats .info p').html(final_str);
-	}
-
-	filterDatasets = function() {
-		var hashOptions = $.deparam.fragment();
-		var filter = "";
-		var tag_count = 0;
-		var format_count = 0;
-		var group_count = 0;
-		cleanTagFormat();
-		cleanGroups();
-		facets['tags'] = []
-		if (hashOptions.tags) {
-			var filters = hashOptions.tags.split('.')
-			for ( var i=1; i<filters.length; i++ ) {
-				pushTagFormat(filters[i], 'tags');
-				facets['tags'].push(filters[i]);
-			}
-			tag_count = filters.length - 1;
-			filter += hashOptions.tags;
-		}		
-		facets['res_format'] = []
-		if (hashOptions.res_format) {
-			var filters = hashOptions.res_format.split('.')
-			for ( var i=1; i<filters.length; i++ ) {
-				pushTagFormat(filters[i], 'res_format');
-				facets['res_format'].push(filters[i]);
-			}
-			format_count = filters.length - 1;
-			filter += hashOptions.res_format;
-		}
-		facets['groups'] = []
-		if (hashOptions.groups) {
-			var filters = hashOptions.groups.split('.')
-			for ( var i=1; i<filters.length; i++ ) {
-				facets['groups'].push(filters[i]);
-				activeGroup(filters[i]);
-			}
-			group_count = filters.length - 1;
-			filter += hashOptions.groups;
-		}
-		updateFilterMessage(tag_count, format_count, group_count);
-		if (hashOptions.query) {
-			if (!hashOptions.filter) {
-				hashOptions.filter = "";
-			}
-			filter += ":contains('" + hashOptions.query + "')"
-			$('#search').val(hashOptions.query);
-		}
-		$('.datasets').isotope( {
-			filter: filter,
-			sortBy : hashOptions.sort 
-		});
-
-	}
-
-	clearFilter = function (eventObject) {
-		var filter = $(eventObject.currentTarget).attr('ckan-filter');
-		facets[filter] = [];
-		closeDropdowns();
-		pushUrl();
-		return false;
-	}
-
-	closeDropdowns = function() {
-		$('[data-toggle="dropdown"]').parent().removeClass('open');
-	}
-
-	toggleFacetToFilter = function (eventObject) {
-		var filter = $(eventObject.currentTarget).attr('ckan-filter');
-		var facet = $(eventObject.currentTarget).attr('ckan-facet');
-		var index = facets[filter].indexOf(facet);
-		if ( index < 0) {
-			facets[filter].push(facet);
-		} else {
-			facets[filter].splice(index, 1);
-		}
-		closeDropdowns();
-		pushUrl();
-		return false;
-	}
-
-	toggleQuery = function(eventObject) {
-		kwd = $(this).val();
-		if ( (kwd != '') && (kwd.length >= 2) ) { 
-			query_str = kwd;
-		} else {
-			query_str = "";
-		}
-		pushUrl();
-		return true;
-	}
-
-	toggleSort = function(eventObject) {
-		sort_str = $(eventObject.currentTarget).attr('ckan-sort');
-		closeDropdowns();
-		pushUrl();
-		return false;
-	}
-
-	$(window).bind( 'hashchange', filterDatasets).trigger('hashchange');
 	$('a[ckan-clear]').click(clearFilter);
 	$('a[ckan-facet]').click(toggleFacetToFilter);
 	$('#search').keyup(toggleQuery) ;
-	$('a[ckan-sort]').click(toggleSort);
-
-	//$('.dropdown-menu').find('form').click(function (e){e.stopPropagation();});
+	$('a[ckan-sort]').click(toggleSort);	
 
 });
 
